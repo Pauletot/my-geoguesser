c74066c2c51cae277e80b92a3e44c30ce7985ae0
Pauletto Matteo <matpau018@gmail.com>
2026-06-03 14:36:36 +0200

    added some pics

--- places.js
+++ places.js
@@ -13,5 +13,35 @@ const places = [
         lon:  13.399413,
         pic: "pic/prova2.jpg"
     },
-    
+
+    {
+        name: "Stellenbosch campus",
+        lat:  -33.9330322,
+        lon:  18.8640395,
+        pic: "pic/stellenbosch.jpg"
+    },
+
+    {
+    name: "oven",
+    lat:  -33.9457107,
+    lon:  18.8873165,
+    pic: "pic/ovenStellenbosch.jpg"
+    },
+{
+    name: "Cape of good hope",
+    lat:  -34.3568182,
+    lon:  18.4741398,
+    pic: "pic/CapeofGoodHope.jpg"
+},    
 ];
+
+/*
+{
+    name: "",
+    lat:  
+    lon:  
+    pic: "pic/.jpg"
+},
+
+
+*/
