74c7200e464717169fe4a3fc3c6b1c9b2cac6ff1
Pauletto Matteo <matpau018@gmail.com>
2026-06-03 11:43:16 +0200

    first commit

---
+++ places.js
@@ -0,0 +1,17 @@
+// 1. PLACES DATABASE - array of objects
+//Every place has: name, coordinates, link at 360° pic
+const places = [
+    {
+        name: "Paris, Place des Vosges",
+        lat: 48.855013,
+        lon: 2.366114,
+        pic: "pic/prova.jpg"
+    },
+    {
+        name: "Berlin",
+        lat:  52.520433,
+        lon:  13.399413,
+        pic: "pic/prova2.jpg"
+    },
+    
+];
